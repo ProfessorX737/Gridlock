88c8f3eb58ac5a199e0516c88922a7778cf36226
Ray <z5060188@ad.unsw.edu.au>
2018-04-27 11:39:07 +1000

    Basic implementation of directed weighted graph

--- src/DirectedWeightedGraph.java
+++ src/DirectedWeightedGraph.java
@@ -1,7 +1,9 @@
 import java.util.Collection;
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.Map;
 import java.util.Set;
+
 
 /**
  * a directed weighted graph implementation 
@@ -16,34 +18,90 @@ public class DirectedWeightedGraph<N> implements Graph<N> {
 		edges = new HashMap<Edge, Integer>();
 	}
 
+	/**
+	 * since this is a directed graph, from is connected to to
+	 * but not the other way around
+	 */
 	@Override
-	public void insert(N a, N b, int weight) {
-		// TODO Auto-generated method stub
-		
+	public void insert(N to, N from, int weight) {
+		if (!connections.containsKey(to)) {
+			connections.put(to, new HashSet<N>());
+		}
+		if (!connections.containsKey(from)) {
+			connections.put(from, new HashSet<N>());
+		}
+		connections.get(from).add(to);
+		edges.put(new Edge(to, from), weight);
+	}
+
+	/**
+	 * Delete node from connections map
+	 * O(n^2) time
+	 */
+	@Override
+	public void delete(N node) {
+		Set<N> allConnection = connections.keySet();
+		//Deleting all directed towards node
+		for (N connection : allConnection) {
+			if (connections.get(connection).contains(node)) {
+				connections.get(connection).remove(node);
+				edges.remove(new Edge(connection, node));
+			}
+		}
+		//Deleting all directed from a
+		for (N connection : connections.get(node)) {
+			edges.remove(new Edge(node, connection));
+		}
+		connections.remove(node);
+	}
+
+	/**
+	 * Checks whether two nodes are connected
+	 */
+	@Override
+	public boolean isConnected(N to, N from) {
+		return connections.get(from).contains(to);
 	}
 
 	@Override
-	public void delete(N a) {
-		// TODO Auto-generated method stub
-		
-	}
-
-	@Override
-	public boolean isConnected(N a, N b) {
-		// TODO Auto-generated method stub
-		return false;
-	}
-
-	@Override
-	public int getWeight(N a, N b) {
-		// TODO Auto-generated method stub
-		return 0;
+	public int getWeight(N to, N from) {
+		return edges.get(new Edge(to, from));
 	}
 
 	@Override
 	public Collection<N> getConnections(N node) {
-		// TODO Auto-generated method stub
-		return null;
+		return connections.get(node);
+	}
+	
+	/**
+	 * edge which represents the edge in the directed graph 
+	 *
+	 */
+	private final class Edge {
+		N to;
+		N from;
+		
+		public Edge(N to, N from) {
+			this.to = to;
+			this.from = from;
+		}
+		
+		@Override
+		public boolean equals(final Object o) {
+			if (this == o) return true;
+			if (o == null) return false;
+			if (getClass() != o.getClass()) return false;
+			Edge other = (Edge) o;
+			return (this.to == other.to && this.from == other.from);
+		}
+		
+		@Override
+		public int hashCode() {
+			int result = 1;
+		    result = 31 * result + to.hashCode();
+		    result = 31 * result + from.hashCode();
+		    return result;
+		}
 	}
 
 }
