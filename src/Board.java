58bb645357d8d341eac5377af3baf728fced83d7
Ray <z5060188@ad.unsw.edu.au>
2018-04-27 12:05:30 +1000

    Use as nodes for the graph
    Board is the board of the game while the gamestate is the current state of the game and will be used as the node for the graph.

---
+++ src/Board.java
@@ -0,0 +1,14 @@
+
+/**
+ * represent the current board of the game
+ */
+public class Board {
+	private final int sizeX;
+	private final int sizeY;
+
+	public Board() {
+		sizeX = 5;
+		sizeY = 5;
+	}
+
+}
---
+++ src/GameState.java
@@ -0,0 +1,45 @@
+import java.util.ArrayList;
+import java.util.Collection;
+
+/**
+ * keep track of the current state of the game 
+ * acts as nodes in the graph
+ *
+ */
+public class GameState {
+	private final Board board;
+	private final int turns;
+	private final Collection<Board> previousMoves;
+
+	/**
+	 * Initialize with no parameters
+	 */
+	public GameState() {
+		board = new Board();
+		turns = 0;
+		previousMoves = new ArrayList<Board>();
+	}
+	/**
+	 * Initialize with parameters
+	 * @param board
+	 * @param turns
+	 * @param previousMoves
+	 */
+	public GameState(Board board, int turns, Collection<Board> previousMoves) {
+		this.board = board;
+		this.turns = turns;
+		this.previousMoves = previousMoves;
+	}
+	
+	Board getBoard() {
+		return board;
+	}
+	
+	int getTurns() {
+		return turns;
+	}
+	
+	Collection<Board> getPreviousMoves() {
+		return previousMoves;
+	}
+}
