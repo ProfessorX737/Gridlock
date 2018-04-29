be58387f4598891eead6a2c0afbe9ff6187870c2
Ray <z5060188@ad.unsw.edu.au>
2018-04-30 00:39:59 +1000

    Implementation of vehicle and board
    Uses the point class which is not intended to be used to store coordinates. Board also uses a matrix of char which could be changed into matrix of indices.

--- src/Board.java
+++ src/Board.java
@@ -1,20 +1,37 @@
+import java.awt.Point;
+import java.util.Collection;
 
 /**
  * represent the current board of the game
  */
 public class Board {
-	private final int sizeX;
-	private final int sizeY;
+	private int sizeX;
+	private int sizeY;
+	private Collection<Vehicle> vehicleList;
 	private char[][] board;
 
-	public Board(int sizeX, int sizeY) {
-		this.sizeX = sizeX;
-		this.sizeY = sizeY;
+	public Board(int sizeX, int sizeY, Collection<Vehicle> vehicleList) {
+		this.vehicleList = vehicleList;//need to fix this as it requires copy constructor
 		board = new char[sizeX][sizeY];
 		for (int y = 0; y < sizeY; y++) {
 			for (int x = 0; x < sizeX; x++) {
 				board[x][y]= '#';
 			}
+		}
+		for (Vehicle vehicle : vehicleList) {
+			Collection<Point> takenPos = vehicle.getTakenPos();
+			for(Point point : takenPos) {
+				//weird stuff will change that
+				board[(int)point.getX()][(int)point.getY()] = (char) (vehicle.getID() + '0');
+			}
+		}
+	}
+	
+	public boolean isOccupied(int row, int col) {
+		if (board[row][col] == '#') {
+			return true;
+		} else {
+			return false;
 		}
 	}
 	
--- src/Vehicle.java
+++ src/Vehicle.java
@@ -1,11 +1,80 @@
+import java.awt.Point;
+import java.util.ArrayList;
+import java.util.Collection;
 
+/**
+ * Represents vehicles on the board 
+ *
+ */
 public class Vehicle {
-	private int id;
-	private boolean isVertical;
-	private int length;
-	private int row;
-	private int col;
-	private String color;
+	private final int id;
+	private final boolean isVertical;
+	private final int length;
+	private final Point pos; //might change this to own class because it doesn't really work
+	private final String color;
 	
-
+	/**
+	 * Constructor for vehicles, takes the parameters
+	 * @post 
+	 * @param id, identifier for the vehicle, should be unique
+	 * @param isVertical, the orientation of the vehicle
+	 * @param length, length of the vehicle, must be positive
+	 * @param row, row coordinate
+	 * @param col, column coordinate
+	 * @param color, color of the car
+	 */
+	public Vehicle(int id, boolean isVertical, int length, int row, int col, String color) {
+		this.id = id;
+		this.isVertical = isVertical;
+		this.length = length;
+		this.pos = new Point(row, col);
+		this.color = color;
+	}
+	
+	public int getID() {
+		return id;
+	}
+	
+	public boolean getIsVertical() {
+		return isVertical;
+	}
+	
+	public int getLength() {
+		return length;
+	}
+	
+	public Point getPosition() {
+		return pos;
+	}
+	
+	public String getColor() {
+		return color;
+	}
+	
+	public Integer getRow() {
+		return (int) pos.getX();
+	}
+	
+	public Integer getCol() {
+		return (int) pos.getY();
+	}
+	
+	/**
+	 * Return a collection of points which represents the positions that are
+	 * taken by the vehicle.
+	 * @return
+	 */
+	public Collection<Point> getTakenPos() {
+		Collection<Point> takenPos = new ArrayList<Point>();
+		if (getIsVertical()) {
+			for (int i = 0; i < length; i++) {
+				takenPos.add(new Point(getRow(), getCol() + i));
+			}
+		} else {
+			for (int i = 0; i < length; i++) {
+				takenPos.add(new Point(getRow() + i, getCol()));
+			}
+		}
+		return takenPos;
+	}
 }
