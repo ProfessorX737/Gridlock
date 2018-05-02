d0007207bb9df608488ce58bd0f976fe858e84da
Xavier Poon <>
2018-05-02 20:47:32 +1000

    added main function

--- BoardTest.java
+++ BoardTest.java
@@ -1,29 +1,32 @@
-import static org.junit.Assert.*;
-
+//import static org.junit.Assert.*;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.HashMap;
 import java.util.Map;
 
-import org.junit.Test;
+//import org.junit.Test;
 
 public class BoardTest {
 
-	@Test
-	public void test() {
+	//@Test
+	public static void test() {
 		Map<Integer, Vehicle> vehicleMap = new HashMap<Integer, Vehicle>();
 		vehicleMap.put(0, new Vehicle(0, true, 2, 0, 0, "Red"));
 		Board foo = new Board(5,5, vehicleMap);
 		foo.showBoard();
 		System.out.println("");
-		assertFalse(foo.checkMove(0, 0, 1));
-		assertTrue(foo.checkMove(0, 2, 1));
+		//assertFalse(foo.checkMove(0, 0, 1));
+		//assertTrue(foo.checkMove(0, 2, 1));
 		foo.moveVehicle(0, 2, 1);
 		foo.showBoard();
 		System.out.println("");
 		foo.moveVehicle(0, 2, 2);
 		foo.showBoard();
 		System.out.println("");
+	}
+	
+	public static void main(String[] args) {
+		test();
 	}
 
 }
