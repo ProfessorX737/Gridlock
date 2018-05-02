b8b05a8917285eee3a8f3aff7396d5e144e892de
Ray <z5060188@ad.unsw.edu.au>
2018-05-02 22:00:36 +1000

    getVehicle(), getMainVehicle, isSolved,

--- BoardTest.java
+++ BoardTest.java
@@ -12,17 +12,19 @@ public class BoardTest {
 	public static void test() {
 		Map<Integer, Vehicle> vehicleMap = new HashMap<Integer, Vehicle>();
 		vehicleMap.put(0, new Vehicle(0, true, 2, 0, 0, "Red"));
-		Board foo = new Board(5,5, vehicleMap);
+		vehicleMap.put(1, new Vehicle(1, false, 2, 3, 0, "Blue"));
+		Board foo = new Board(5, 5, 4, 0, vehicleMap);
 		foo.showBoard();
 		System.out.println("");
 		//assertFalse(foo.checkMove(0, 0, 1));
 		//assertTrue(foo.checkMove(0, 2, 1));
-		foo.moveVehicle(0, 2, 1);
+		foo.moveVehicle(0, 2, 0);
 		foo.showBoard();
 		System.out.println("");
-		foo.moveVehicle(0, 2, 2);
+		foo.moveVehicle(0, 3, 0);
 		foo.showBoard();
 		System.out.println("");
+		System.out.println(foo.isSolved());
 	}
 	
 	public static void main(String[] args) {
