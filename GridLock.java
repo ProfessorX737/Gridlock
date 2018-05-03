5fca6a7d534932b443ef04abff9126fb6c843146
Xavier Poon <>
2018-05-04 00:44:25 +1000

    added grid lock class, did nothing basically

---
+++ GridLock.java
@@ -0,0 +1,15 @@
+import java.awt.BorderLayout;
+import javax.swing.JFrame;
+
+public class GridLock{
+	public static void main(String args[]) {
+		JFrame f = new JFrame("GridLock");
+		f.setLayout(new BorderLayout());
+		
+		
+		
+		f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		f.pack();
+		f.setVisible(true);
+	}
+}
