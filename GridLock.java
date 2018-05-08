740f7d95d6d73ef5222bf9c48b199be0c5efc5dd
Unknown <jerryzhu96@hotmail.com>
2018-05-08 20:25:33 +1000

    Added 2nd JPanel for the Side UI
    GUI semi complete, no event handlers written yet

--- GridLock.java
+++ GridLock.java
@@ -1,29 +1,40 @@
-import java.awt.BorderLayout;
-import java.awt.Color;
+import java.awt.*;
 
-import javax.swing.JFrame;
+import javax.swing.*;
+import javax.swing.border.Border;
 
 public class GridLock{
 	public static void main(String args[]) {
 		JFrame f = new JFrame("GridLock");
 		f.setLayout(new BorderLayout());
-		f.setLayout(new BorderLayout());
+
 		f.setBackground(Color.BLACK);
-		PuzzleGame puzzleGame = new PuzzleGame(6,6,2,5);
-		puzzleGame.addVehicle(false, 2, 2, 0, Color.RED);
-		puzzleGame.addVehicle(false, 3, 0, 0, Color.ORANGE);
-		puzzleGame.addVehicle(true, 3, 1, 2, Color.ORANGE);
-		puzzleGame.addVehicle(true, 2, 3, 0, Color.ORANGE);
-		puzzleGame.addVehicle(false, 3, 5, 0, Color.ORANGE);
-		puzzleGame.addVehicle(true, 2, 4, 4, Color.ORANGE);
-		puzzleGame.addVehicle(false, 2, 3, 4, Color.ORANGE);
-		puzzleGame.addVehicle(true, 3, 0, 5, Color.ORANGE);
-		PuzzleView bv = new PuzzleView(puzzleGame,50);
-		PuzzleController bc = new PuzzleController(puzzleGame,bv);
+
+		JPanel container = new JPanel();
+		ButtonPanel panelTwo = new ButtonPanel(container);
+		panelTwo.add(new JLabel("2"));
+
+		Game game = new Game(6,6,2,4);
+		game.addVehicle(true, 2, 3, 3, Color.BLUE);
+		game.addVehicle(false, 2, 2, 1, Color.RED);
+		game.addVehicle(true, 2, 0, 4, Color.GREEN);
+		game.addVehicle(false, 3, 0, 0, Color.PINK);
+		BoardView bv = new BoardView(game,50);
+
+		BoardController bc = new BoardController(game,bv);
+
+		container.setLayout(new GridLayout(1,2));
+		container.add(bv,BorderLayout.CENTER);
+		container.add(panelTwo, BorderLayout.EAST);
 		bv.setController(bc);
-        f.add(bv,BorderLayout.CENTER);
+//        f.add(bv,BorderLayout.CENTER);
+		f.add(container);
 		f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		f.pack();
+
+		// Button ui
+
+
 		f.setVisible(true);
 	}
 }
