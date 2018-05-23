742a15a5a37d72dfa7d660299aa6e1f2f0ff01ec
Unknown <jerryzhu96@hotmail.com>
2018-05-23 20:49:06 +1000

    Removed classes which are not directly related to networking

--- Heuristic.java
+++
@@ -1,7 +0,0 @@
-public interface Heuristic<N> {
-    /**
-     * @param node, the node from which to calculate the heuristic
-     * @return the heuristic value of the node
-     */
-    int getH(N node);
-}
