188bef2ba4a7ebbd4322af25e77155d3fa2c1a90
eveHan <eveunsw@sina.com>
2018-05-25 16:24:21 +1000

    added the animation

--- GridLock.java
+++ GridLock.java
@@ -4,11 +4,19 @@ import java.awt.*;
 public class GridLock {
     
     public static void test0() {
+    		JFrame f = new JFrame("GridLock");
+		f.add(new Animation(f));
+		f.setSize(500, 400);                       // set the size of the window
+        f.setLocationRelativeTo(null);             // set the window to center of the screen
+        f.setVisible(true);                        // indicate window
+        f.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE); // exit program when click the close button of window
+    	/*
 		GridlockGame game = new GridlockGame();
 		game.generatePuzzlesInBackground();
 		MainMenu menu = new MainMenu();
 		MenuController menuContr = new MenuController(game,menu);
 		menu.setController(menuContr);
+	*/
     }
 
     public static void main(String args[]) {
